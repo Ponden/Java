6a71ceb795b84e2a41b7fefd42f84a7cbdefbd6a
Ponden <lshumat2@uncc.edu>
2015-11-01 21:30:26 -0500

    Update FastaFileParser.java

--- FastaFileParser.java
+++ FastaFileParser.java
@@ -1,6 +1,7 @@
 import java.util.*;
 import java.io.*;
 
+
 public class FastaFileParser 
 		//initialize header and sequence
 {		private String header;
@@ -13,7 +14,7 @@ public class FastaFileParser
 			this.sequence=sequence;
 		}
 
-		public static List <FastaFileParser> readFile(String file) throws Exception
+		public static List <FastaFileParser> readFile(File file) throws Exception
 		{	//parse the headers and sequence information into an arrayList 		
 			List<FastaFileParser> getList = new ArrayList<FastaFileParser>();
 			BufferedReader reader = new BufferedReader(new FileReader(file));
@@ -31,7 +32,7 @@ public class FastaFileParser
 					getList.add(new FastaFileParser(fileHeader,fileSequence));
 				}
 			}
-			reader.close(); //gotta close bc reasons.
+			reader.close();
 			return getList;
 		}
 
@@ -57,17 +58,75 @@ public class FastaFileParser
 			return (float)gcContent/(float)length;
 		}
 
+		public static void writeUnique(File inFile, File outFile) throws Exception
+		{
+			List<FastaFileParser> fastaList = FastaFileParser.readFile(inFile);
+		
+			//Storing sequences and their respective counts in a hashmap
+			HashMap<String, Integer> uniqSeqs = new HashMap<String, Integer>();
+			
+			for(FastaFileParser fs : fastaList)
+			{			
+				Integer count = uniqSeqs.get(fs.getSequence());
+				if( count == null )
+				{
+					count = 0;
+				}
+				count++;
+				uniqSeqs.put(fs.getSequence(), count);
+			}
+			
+			Map<String, Integer> uniSeqsSorted = sortMap(uniqSeqs);
+			BufferedWriter writeFile = new BufferedWriter(new FileWriter(outFile));
+			for( Map.Entry<String, Integer> x : uniSeqsSorted.entrySet())
+			{
+				String key = x.getKey();
+				Integer value = x.getValue();
+				writeFile.write(">" + value + "\n" + key + "\n");
+				writeFile.flush();
+			}
+			writeFile.close();
+		}
+		
+		private static HashMap sortMap(HashMap map)
+		{
+			List newList = new LinkedList(map.entrySet());
+			Collections.sort
+			(
+				newList, new Comparator()
+				{
+					public int compare(Object o1, Object o2)
+					{
+						return ( (Comparable) ((Map.Entry)(o1)).getValue()).compareTo(((Map.Entry)(o2)).getValue());
+					}
+				}
+			);
+			HashMap sortedHashMap = new LinkedHashMap();
+			for( Iterator x = newList.iterator(); x.hasNext(); )
+			{
+				Map.Entry entry = (Map.Entry)x.next();
+				sortedHashMap.put(entry.getKey(), entry.getValue());
+			}
+			return sortedHashMap;	
+			
+		}
+		
 		public static void main(String[] args) throws Exception{
 			
-			List <FastaFileParser> fastaList = FastaFileParser.readFile("/Users/Ponden/Documents/Fall 2015/Programming 3/samplefasta.fa");
+			File inFile = new File ("/Users/Ponden/Documents/Fall 2015/Programming 3/samplefasta.fa");
+			File outFile = new File ("/Users/Ponden/Documents/Fall 2015/Programming 3/unique_seq_count.fa");
+
+			List <FastaFileParser> fastaList = FastaFileParser.readFile(inFile);
 			
 			for (FastaFileParser seq : fastaList)
 			{
 				System.out.println(seq.getHeader());
 				System.out.println(seq.getSequence());
 				System.out.println(seq.getGCRatio());
-				System.out.println(seq.getLength());
-			}
+				System.out.println(seq.getLength());	
+			} 
+			
+			writeUnique(inFile, outFile);
 		}
 
 }
